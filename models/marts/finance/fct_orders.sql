020726b7fa064cfbfd7f7300a22a5d9b6856ae7d
Tadas Paulikas <tadpaulikas@gmail.com>
2026-04-14 09:20:22 +0000

    Suffling files and folders for better understanding of dataset.

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,9 +1,9 @@
 with orders as  (
-    select * from {{ ref ('stg_dbt_project__orders' )}}
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
 
 payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
+    select * from {{ ref ('stg_stripe__payment') }}
 ),
 
 order_payments as (
--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('stripe', 'payments') }}
+    select * from {{ source('stripe', 'payment') }}
 
 ),
 
